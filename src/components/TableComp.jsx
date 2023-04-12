bf00bd8b4a1917a827f582a8b5e40fda94855e2b
Josue Reyes Rivera <li_josuereyes@unca.edu.mx>
2023-04-12 01:07:10 -0500

    Mejoramiento de TableComp

--- src/components/TableComp.jsx
+++ src/components/TableComp.jsx
@@ -6,6 +6,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Tooltip } from 'antd';
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -17,15 +28,14 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     },
   }));
 
-import RowsComp from './RowsComp';
 import { Empty } from 'antd';
 
-const TableComp = ( { data, titles, haveId, widths, events } ) => {
+const TableComp = ( { data, titles, widths, events } ) => {
     return (
     data.length == 0
         ?   <Empty description={ <h5>Sin datos</h5> } />
         :   <TableContainer component={Paper} >
-                <Table /* sx={{ minWidth: 700 }} */ aria-label="customized table" >
+                <Table aria-label="customized table" >
                     <TableHead>
                         <TableRow>
                             {
@@ -37,9 +47,37 @@ const TableComp = ( { data, titles, haveId, widths, events } ) => {
                     </TableHead>
                     <TableBody>
                         {
-                            data.map( (row) => (
-                                <RowsComp key={row.id} row={row} haveId={haveId} events={events} />
-                            ))
+                            data.map( (row) => {
+                                let rowId = row.id
+                                row = Object.values(row)
+                                row.shift()
+
+                                return (
+                                    <StyledTableRow style={{ height:'30px' }} key={rowId} >
+                                        {
+                                            row.map((cell, index) => (
+                                                <StyledTableCell key={ index } style={{ padding: '7px' }} > {cell} </StyledTableCell>
+                                            ))
+                                        }
+                                        {
+                                            <StyledTableCell style={{ padding: '5px 0', display:'flex' }} >
+                                                {
+                                                    events.map(({variant, onclick, icon, tooltip},index) => (
+                                                        <Tooltip title={tooltip[0]} placement={tooltip[1]} key={index} >
+                                                            <button 
+                                                                type="button"  
+                                                                className={`d-flex btn btn-${variant} me-2`} 
+                                                                style={{ minWidth: '28px', padding:'2px'}}
+                                                                onClick={ () => onclick(rowId)}
+                                                            > {icon} </button>
+                                                        </Tooltip>
+                                                    ))
+                                                }
+                                            </StyledTableCell>
+                                        }
+                                    </StyledTableRow>
+                                )
+                            })
                         }
                     </TableBody>
                 </Table>
